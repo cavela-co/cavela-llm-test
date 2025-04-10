3c359bf148856b390fe77cd621bea6159ae3ebb5
Abraham Quintero <abraham@cavela.com>
2025-04-10 19:08:03 -0400

    Add type

--- api/models.py
+++ api/models.py
@@ -13,10 +13,18 @@ class Task(models.Model):
         return self.title
 
 
+class FileType(models.TextChoices):
+    QUOATATION = "QUOATATION"
+    PROD_SPEC = "PROD_SPEC"
+    QA_REPORT = "QA_REPORT"
+    OTHER = "OTHER"
+
+
 class UploadedFile(models.Model):
     file = models.FileField(upload_to="uploads/")
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
+    type = models.CharField(max_length=20, choices=FileType.choices, null=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
